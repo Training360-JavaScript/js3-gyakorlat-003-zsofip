e462be86b2e1fc36baf0b1df543365600ee5c583
zsofip <91950899+zsofip@users.noreply.github.com>
2021-11-14 15:57:28 +0100

    done

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,1 +1,10 @@
+'use strict';
 
+const setCookie = (name, value) => {
+    const now = new Date();
+    now.setTime(now.getTime() + (15 * 60 * 1000));
+    const expires = now.toUTCString();
+    document.cookie = `${name}=${value};expires=${expires}`;
+}
+
+export default setCookie;
